f8fb4e97f630354b76ad1adb02465ec5c227da4d
Simone Benatti <simone.benatti@studenti.unipr.it>
2020-02-11 13:51:35 +0100

    Silly mistake. Back to relative paths

--- contrib/packaging-python/gitlab/build_pychrono.bat
+++ contrib/packaging-python/gitlab/build_pychrono.bat
@@ -3,6 +3,6 @@ echo Process begins
 call C:\Users\buildbot\Miniconda3\Scripts\activate.bat
 call conda install --yes conda-build anaconda-client
 call conda install --yes -c intel mkl-devel=2018.0.3
-call conda build C:\codes\Chrono\Chrono_Source\contrib\packaging-python\conda --python=3.6 --no-remove-work-dir --dirty
+call conda build .\contrib\packaging-python\conda --python=3.6 --no-remove-work-dir --dirty
 call anaconda --token "%ANACONDA_TOKEN%" upload C:\Users\buildbot\Miniconda3\envs\base\win-64\pychrono-4.0.0*.bz2
 echo "End Reached"
