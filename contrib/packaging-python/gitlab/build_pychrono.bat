ed15248707dc4d2e501df31c1c709bd9b499b1ee
Simone Benatti <simone.benatti@studenti.unipr.it>
2020-02-11 15:47:30 +0100

    Use conda install location var

--- contrib/packaging-python/gitlab/build_pychrono.bat
+++ contrib/packaging-python/gitlab/build_pychrono.bat
@@ -1,6 +1,6 @@
 @echo on
 echo Process begins
-call C:\Users\buildbot\Miniconda3\Scripts\activate.bat
+call "%CONDA_INSTALL_LOCN%"\activate.bat
 call conda install --yes conda-build anaconda-client
 call conda install --yes -c intel mkl-devel=2018.0.3
 call conda build .\contrib\packaging-python\conda --python=3.6 --no-remove-work-dir --dirty
