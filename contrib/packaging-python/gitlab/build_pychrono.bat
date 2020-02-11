9adb2f30c0a4e3c451bfd9942698d3b72ade388f
Simone Benatti <simone.benatti@studenti.unipr.it>
2020-02-11 15:01:13 +0100

    Directly use PS

--- contrib/packaging-python/gitlab/build_pychrono.bat
+++ contrib/packaging-python/gitlab/build_pychrono.bat
@@ -4,5 +4,5 @@ call C:\Users\buildbot\Miniconda3\Scripts\activate.bat
 call conda install --yes conda-build anaconda-client
 call conda install --yes -c intel mkl-devel=2018.0.3
 call conda build .\contrib\packaging-python\conda --python=3.6 --no-remove-work-dir --dirty
-call anaconda --token "%ANACONDA_TOKEN%" upload C:\Users\buildbot\Miniconda3\envs\base\win-64\pychrono-4.0.0*.bz2
+call anaconda --token "%ANACONDA_TOKEN%" upload C:\Users\buildbot\Miniconda3\conda-bld\pychrono-4.0.0*.bz2
 echo "End Reached"
